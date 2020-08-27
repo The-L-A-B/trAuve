728624b924070d666c349cf9151eb38c06820a14
Kisha Prudente <kisha.prudente@gmail.com>
2020-08-27 12:50:37 -0400

    renamed database name and db password properly

--- config/config.js
+++ config/config.js
@@ -1,26 +1,28 @@
+require('dotenv').config();
+
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": process.env.DB_PASS,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  development: {
+    username: 'root',
+    password: process.env.DB_PASS,
+    database: 'trauve',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": process.env.DB_PASS,
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  test: {
+    username: 'root',
+    password: process.env.DB_PASS,
+    database: 'trauve_testdb',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": process.env.DB_PASS,
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  }
-}
+  production: {
+    username: 'root',
+    password: process.env.DB_PASS,
+    database: 'trauve_proddb',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
+  },
+};
