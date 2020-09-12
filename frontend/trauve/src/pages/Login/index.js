a9d2e5f6ad1ea75f5582f6a7d424c0d774858e76
Jiyoon Kim <jiyoon9886@gmail.com>
2020-09-12 02:28:26 -0400

    started styling for Login and added Header

--- frontend/trauve/src/pages/Login/index.js
+++ frontend/trauve/src/pages/Login/index.js
@@ -5,22 +5,32 @@ import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
+import Header from '../../components/Header';
 
+const useStyles = makeStyles({
+  root: {
+    marginTop: '80px',
+    color: 'black',
+  },
+});
 
-const Login = (props)=>{
-    return(
-        <div>
-            <Paper>
-                <TextField id="outlined-basic" label="USERNAME" variant="outlined" />
-                <TextField
-                id="outlined-password-input"
-                label="PASSWORD"
-                type="password"
-                autoComplete="current-password"
-                variant="outlined"
-                />
-            </Paper>
-        </div>
-    )
-}
-export default Login
+const Login = (props) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Header></Header>
+      <Paper className={classes.root}>
+        <TextField id='outlined-basic' label='USERNAME' variant='outlined' />
+        <TextField
+          id='outlined-password-input'
+          label='PASSWORD'
+          type='password'
+          autoComplete='current-password'
+          variant='outlined'
+        />
+      </Paper>
+    </div>
+  );
+};
+export default Login;
