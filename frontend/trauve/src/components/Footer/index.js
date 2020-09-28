cd87afae5a4bcd39918981a2a6259c84b9c51709
kwu3 <kwu3@villanova.edu>
2020-09-28 00:27:38 -0400

    Added styles to footer

--- frontend/trauve/src/components/Footer/index.js
+++ frontend/trauve/src/components/Footer/index.js
@@ -1,28 +1,52 @@
 import React from 'react';
-import { useTheme } from '@material-ui/core/styles';
+import { useTheme, makeStyles } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { Grid, Typography, Divider } from '@material-ui/core';
+import { Grid, Container, Typography, Divider } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    background: '#303B97',
+    color: '#CCCCCC',
+    height: '20vh',
+    width: '100vw',
+  },
+  container: {
+    maxHeight: '10vh',
+  },
+  divider: {
+    width: '80%',
+    margin: 'auto',
+  },
+  love: {
+    fontSize: '0.5rem',
+  },
+});
 
 const Footer = () => {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up('sm'));
 
+  const classes = useStyles();
+
   return (
-    <Grid container justify='center' alignItems='center'>
-      <Grid container item xs={12}>
+    <Grid container alignItems='flex-end' className={classes.root}>
+      <Grid container item xs={12} spacing={2} alignItems='stretch'>
         <Grid item sm xs={12} align='center'>
-          nav links
+          <Container className={classes.container}>nav links</Container>
         </Grid>
         <Divider
           flexItem={matches}
           orientation={matches ? 'vertical' : 'horizontal'}
+          className={matches ? null : classes.divider}
         />
         <Grid item sm xs={12} align='center'>
-          school contact info
+          <Container className={classes.container}>
+            school contact info
+          </Container>
         </Grid>
       </Grid>
-      <Grid item xs={12} justify='center' align='center'>
-        <Typography variant='overline' gutterBottom>
+      <Grid item xs={12} align='center'>
+        <Typography variant='overline' gutterBottom className={classes.love}>
           Made with ♡ by L.A.B.
         </Typography>
       </Grid>
